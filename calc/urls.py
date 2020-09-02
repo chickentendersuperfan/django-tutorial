dba93aa2c43d28dea0c417f3c7919350a5bcd804
Nathan <nathannhek@csu.fullerton.edu>
2020-09-02 16:37:15 -0700

    Remove module

--- calc/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views 
-
-# App URLs
-urlpatterns  = [
-    path('', views.home, name='home'), # for the homepage call the function called name="home"
-    path('add', views.add, name='add')
-]
